0dbb5ce6b3ae74837b1a2f394c048f8d23ea1e8a
Raflirpl1 <144410191+Raflirpl1@users.noreply.github.com>
2023-10-18 11:39:06 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,31 @@
+<?Php
+$koneksi = new mysqli('localhost', 'root', '', 'rafli')
+    or die(mysqli_error($koneksi));
+
+
+if (isset($_POST['simpan'])) {
+    $idPasien = $_POST['idPasien'];
+    $nmPasien = $_POST['nmPasien'];
+    $jk = $_POST['jk'];
+    $alamat = $_POST['alamat'];
+    $koneksi->query("INSERT INTO pasien (idPasien, nmPasien, jk, alamat) values ('$idPasien', '$nmPasien', '$jk', '$alamat')");
+    
+    header('location:pasien.php');
+  }
+
+if (isset($_GET['idpasien'])) {
+    $idPasien = $_GET['idPasien']; 
+    $koneksi->query("DELETE FROM pasien where idPasien = '$idPasien'");
+    header('location:pasien.php');
+  }
+
+if (isset($_POST['edit'])) {
+    $idPasien = $_POST['idPasien'];
+    $nmPasien = $_POST['nmPasien'];
+    $jk = $_POST['jk'];
+    $alamat = $_POST['alamat'];
+
+    $koneksi->query("UPDATE pasien SET idPasien= '$idPasien', nmPasien= '$nmPasien', jk= '$jk', alamat= '
+    
+    header('location:pasien.php");
+  }
